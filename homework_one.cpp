f27dd7ca100e58fb89748e8e8a58c9acee56150a
Michael Wang <mw3714@nyu.edu>
2018-07-06 15:45:45 +0000

    First Commit.

--- homework_one.cpp
+++ homework_one.cpp
@@ -1,8 +1,30 @@
 #include "std_lib_facilities.h"
 
 int main(){
+  cout << "double conversions\n";
+  double Double = 11.234;
   //double to int
-  double a = 1.2;
-  int b = a;
-  cout << b;
+  int Integer = Double;
+  cout << Integer << "\n";
+  //double to char
+  char Character = Double;
+  cout << Character << "\n";
+  //double to bool
+  bool Bool = Double;
+  cout << Bool << "\n";
+
+  cout << "integer conversions\n";
+  Integer = 2000000;
+  //int to char
+  Character = Integer;
+  cout << Character << "\n";
+  //int to bool
+  Bool = Integer;
+  cout << Bool << "\n";
+
+  cout << "character conversions\n";
+  Character = 'd';
+  //char to bool
+  Bool = Character;
+  cout << Bool << "\n";
 }
