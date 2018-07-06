170fe43e25186814d9c8f42b764c6717978b4785
Michael Wang <mw3714@nyu.edu>
2018-07-06 14:32:05 +0000

    Second Commit

---
+++ repWords.cpp
@@ -0,0 +1,15 @@
+#include "std_lib_facilities.h"
+
+int main(){
+  int numberOfWords = 0;
+  string previous = " ";
+  string current;
+  while(cin >> current){
+    ++ numberOfWords;
+    if(previous == current){
+      cout << "Word number " << numberOfWords << " repeated: " << current << "\n";
+      break;
+    }
+    previous = current;
+  }
+}
